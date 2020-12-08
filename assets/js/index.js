0597c4910e6a4361aa2cae16805bee5a78add738
Farhan <farhan@Farhan-MacBook-Air.local>
2020-12-08 13:57:26 +0800

    Update Login Dll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,73 +1,14 @@
 $(function() {
 
 
-    var map;
-    let isAdd = false;
-    var newMarker = [];
-    var newLine = [];
-    var geojson;
-    let firstLine = false;
-    let counter = 0;
-    let lineColor = 'red';
     let menuOpen = true;
     let isDrag = false;
-
+    let up = 0;
+    let counterUp = 0;
+    let counterDown = 1;
     
 
-    loadMap()
-
-
-    map.on('click', function(e) {
-        // document.getElementById('info').innerHTML =
-        // e.point is the x, y coordinates of the mousemove event relative
-        // to the top-left corner of the map
-        // JSON.stringify(e.point) +
-        // '<br />' +
-        // e.lngLat is the longitude, latitude geographical position of the event
-        // JSON.stringify(e.lngLat.wrap());
-        if (isAdd) {
-            setMarker(e.lngLat.lng, e.lngLat.lat)
-        }
-
-    });
-
-    $('#button-add').on('click', function(e) {
-        isAdd = !isAdd;
-        $('#addLayout').addClass('goRight');
-        $('#undoLayout').removeClass('goRight');
-        $('.layers-select').css('display', 'none')
-        $('.side-menu .card .card-body').append(`
-    <div class="loading-display spinner-grow text-success" role="status">
-        <span class="sr-only">Loading...</span>
-    </div>
-    <div class="loading-display spinner-grow text-success" role="status">
-        <span class="sr-only">Loading...</span>
-    </div>
-    <div class="loading-display spinner-grow text-success" role="status">
-        <span class="sr-only">Loading...</span>
-    </div>
-
-
-    `)
-        setTimeout(() => {
-            $('.loading-display').remove();
-            $('.marker-add').css('display', 'block');
-        }, 500)
-    })
-
-    $('#button-undo').on('click',function(e) {
-        undoLine()
-    })
-    $('#button-delete').on('click',function(e) {
-        deleteMarker()
-        console.log('asd')
-    })
-    $('#colorpicker').farbtastic(function(color) {
-        lineColor = color;
-        console.log(lineColor)
-        map.removeLayer('line').removeSource('line')
-        generateLine()
-    })
+   
     $('.card-header button').on('click', function(e) {
         toggleMenu()
     })
@@ -94,116 +35,50 @@ $(function() {
             },500)
         }
     });
+
+    $('.input-form').on('focus', function(e) {
+        $('#search-layout .input-group').css('width','300px')
+        $('#search-layout').css('width','400px')
+    })
+
+    $('.input-form').on('focusout', function(e) {
+        $('#search-layout .input-group').css('width','150px')
+        $('#search-layout').css('width','250px')
+    })
+
+    $('.item-up').on('click',function(e) {
+        $('.item-menu').css('top',`-${up+=60}px`);
+        console.log(up)
+        let firstItem = $('.item-active')[counterUp];
+        let newItem = $(firstItem).clone();
+        $(newItem).addClass('new')
+        $('.list-menu').append(newItem)
+        counterUp++;
+    })
+
+    $('.item-down').on('click',function(e) {
+            $('.item-menu').css('top',`-${up-=60}px`);
+            console.log(-up)
+
+            let lastItem = $('.item-menu').last();
+            
+
+            
+
+            // let newItem = $(lastItem).clone();
+            // $('.list-menu').prepend(newItem)
+            $(lastItem).remove()
+            counterUp--;
+        
+        
+    })
+
     
 
 
 
-    // all function
-    function loadMap() {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZmhhbjA0MzEiLCJhIjoiY2tpOW83b2kwMDRhMTJybXMwMjB6ZmVzNiJ9.7UQSqyJrky5ltWA5mMuzQw'
-        map = new mapboxgl.Map({
-            container: 'map',
-            center: [120.0203, -3.7739],
-            style: 'mapbox://styles/mapbox/streets-v11',
-            zoom: 9
-        })
-    }
 
-
-    function setMarker(lng, lat) {
-
-
-
-        // var el = document.createElement('div');
-        // el.className = 'marker';
-        // el.style.backgroundImage = 'url(' + img_url + ')';
-
-        newMarker[counter++] = new mapboxgl.Marker()
-            .setLngLat([lng, lat])
-            .addTo(map)
-        
-        
-        newLine.push([lng, lat])
-        if(firstLine)
-        {
-            map.removeLayer('line').removeSource('line');
-        }
-        setLineCoord()
-        
-    }
-
-    function setLineCoord() {
-        geojson = {
-            'type': 'FeatureCollection',
-            'features': [{
-                'type': 'Feature',
-                'properties': {},
-                'geometry': {
-                    'coordinates':newLine,
-                    'type': 'LineString'
-                }
-            }]
-        };
-
-        generateLine()
-    }
-
-    function generateLine()
-    {
-        map.addSource('line', {
-            type: 'geojson',
-            lineMetrics: true,
-            data: geojson
-        });
-
-        // the layer must be of type 'line'
-        map.addLayer({
-            type: 'line',
-            source: 'line',
-            id: 'line',
-            paint: {
-                'line-color': lineColor,
-                'line-width': 1,
-            },
-            layout: {
-                'line-cap': 'round',
-                'line-join': 'round'
-            }
-        });
-
-        firstLine = true;
-    }
-
-    function undoLine() {
-        if(newLine.length > 0 )
-        {
-            newLine.pop()
-        }
-        if(newMarker.length > 0)
-        {
-            newMarker[newMarker.length - 1].remove()
-            newMarker.pop()
-        }
-        
-        if(firstLine)
-        {
-            map.removeLayer('line').removeSource('line');
-        }
-        setLineCoord()
-    }
-
-    function deleteMarker() {
-        if(newLine.length > 0)
-        {
-            map.removeLayer('line').removeSource('line');
-            newMarker.forEach(function(i) {
-                i.remove()
-                newMarker = []
-            })
-            firstLine = false;
-            newLine = [];
-        }
-    }
+   
 
     function toggleMenu() {
 
